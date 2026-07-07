0eb112a07bd5396a57af5c92210042677eb45ad9
govarthini64 <govarthini6464@gmail.com>
2026-07-07 14:46:40 +0530

    Update bmi project.py

--- bmi project.py
+++ bmi project.py
@@ -1,16 +1,23 @@
-weight = float(input("Enter your weight(kg):"))
-height = float(input("Enter your height(cm):"))
+try:
+    weight = float(input("Enter your weight(kg):"))
+    height = float(input("Enter your height(cm):"))
 
-height = height/100
-BMI = weight/(height*height)
+    if (weight <= 0) or (height <= 0):
+        print("Invaild input")
+    else:
+        height = height/100
+        BMI = weight/(height*height)
+        
+        print(f"Your BMI is: {BMI:.2f}")
 
-print(f"Your BMI is: {BMI:.2f}")
-
-if BMI < 18.5:
-    print("Underweight")
-elif 18.5 <= BMI <= 24.9:
-    print("Normal weight")
-elif 25.0 <= BMI <= 29.9:
-    print("Over weight")
-else:
-    print("Obese")
+        if BMI < 18.5:
+            print("Underweight")
+        elif 18.5 <= BMI <= 24.9:
+            print("Normal weight")
+        elif 25.0 <= BMI <= 29.9:
+            print("Over weight")
+        else:
+            print("Obese")
+except ValueError:
+    print("ERROR:Invaild input.Please enter number only..")
+    
